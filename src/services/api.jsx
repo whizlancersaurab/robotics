bf10791ff98d9e5ac97617c08c569f1e8d9d92cf
saurabh <saurabh.agrahari@whizlancer.com>
2025-12-10 18:15:12 +0530

    some error fix

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api` || "http://localhost:3000/api",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
