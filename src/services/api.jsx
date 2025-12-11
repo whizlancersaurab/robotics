03c1b2fac23006a56c2d77d0fe8423dd8d4009d9
saurabh <saurabh.agrahari@whizlancer.com>
2025-12-11 11:52:07 +0530

    some improvements

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,15 +1,15 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL}/api`
+  : "http://localhost:3000/api";
+
 
 export const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api` || "http://localhost:3000/api",
+  baseURL: BASE_URL,
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
 
-
-
-export const SubscribeCompany = (data) => api.post('/subscribe', data)
-export const ContactUs = (data) => api.post('/contactus', data)
-
-
+export const SubscribeCompany = (data) => api.post("/subscribe", data);
+export const ContactUs = (data) => api.post("/contactus", data);
